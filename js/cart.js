6597373fbcfd48657ed43d9d713bd8b2e975e9f4
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-07-09 11:16:15 +0300

    added the cart page

---
+++ js/cart.js
@@ -0,0 +1,77 @@
+fetch(`http://localhost:3000/cart`)
+.then(res=>res.json())
+.then(cartData=>{
+    displayCart(cartData)
+})
+
+//get div elements
+const cartDisplay = document.getElementById('toys-display');
+const price = document.getElementById('total');
+
+function displayCart(cartData){
+    cartData.forEach(cartObj => {
+        let cartCard = document.createElement('div');
+        cartCard.className = "toy-card";
+
+        //get the data from the cart object
+        const toyName = cartObj.name;
+        const toyPrice = cartObj.price;
+        const toyImage = cartObj.image;
+        const description = cartObj.description;
+        const toyId = cartObj.id;
+        const toyQuantity = cartObj.quantity;
+
+        cartCard.innerHTML=`<div class="toy-details" id="toy-name">Ksh: ${toyName} /-</div>
+                            <img src = "${toyImage}" class="toy-image">
+                            <div class="toy-details" id="toy-price">Ksh: ${toyPrice} /-</div>
+                            <div class="toy-details" id="toy-quantity">Qunatity: ${toyQuantity}</div>
+                            <button type="button" class="btn" id="view-desc">View Description</button>
+                            <div class="desc">${description}</div>
+                            <button type="button" class="btn" id="rmv-toy">Remove From Cart</button>`
+        
+        // show and hide toy description
+        const showBtn = cartCard.querySelector('#view-desc');
+        showBtn.addEventListener('click', (e)=>{
+            const desc = cartCard.querySelector('.desc');
+            if (desc.style.display==="none"){
+                desc.style.display = "block";
+                showBtn.textContent = "Hide description";
+            }else{
+                desc.style.display = "none";
+                showBtn.textContent = "View Description";
+            }
+        })
+        
+        // function to remove toy
+        function removeToy(cartObj){
+            fetch(`http://localhost:3000/cart/${toyId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json'
+                },
+                body: JSON.stringify(cartObj)
+            })
+            .then(res=>res.json())
+            
+        }
+        // remove toy from cart
+        const rmvBtn = cartCard.querySelector('#rmv-toy');
+        rmvBtn.addEventListener('click', ()=>{
+
+            let choice = confirm('Are you sure you want to remove item from cart?');
+            console.log(choice)
+            if(choice===true){
+                cartCard.remove();
+                removeToy(cartObj);
+                
+            }
+            
+        })
+        //append cart card to the html document
+        cartDisplay.appendChild(cartCard);
+    });
+
+    // calculate the total price of the cart items
+    //cartData.reduce()
+}
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const toysDisplay = document.getElementById("toys-display");
+let updateComments = [];
 
 fetch(`http://localhost:3000/toys`)
   .then((res) => res.json())
@@ -7,6 +8,23 @@ fetch(`http://localhost:3000/toys`)
   });
 
 function displayToys(toysData) {
+
+  fetch(`http://localhost:3000/cart`)
+  .then((res) => res.json())
+  .then((toysData) => {
+    //console.log(toysData);
+    cartToys(toysData)
+  });
+  let cartToyName;
+  function cartToys(cartToysArr){
+    cartToysArr.forEach(cartToy=>{
+      cartToyName = cartToy.name; 
+      //console.log(cartToyName)
+    })
+    
+    return cartToyName;
+  }
+  
   toysData.forEach((toy) => {
     const toyCard = document.createElement("div");
     toyCard.classList = "toy-card";
@@ -14,34 +32,141 @@ function displayToys(toysData) {
     //let shortDescription = toy.shortDescription;
     let toyImage = toy.image;
     let toyPrice = toy.price;
+    let longDescription = toy.longDescription;
+    let toyId = toy.id;
+    let toyComments = toy.comments;
 
-    toyCard.innerHTML = `<img src = "${toyImage}" class="toy-image">
-                             <div class="toy-details" id="toy-price">Ksh: ${toyPrice} /-</div>
-                             <div class="toy-details" id="toy-name">${toyName}</div>
-                             <button type="button" class="btn" id="view-toy">View Toy</button>
-                             <button type="button" class="btn" id="add-to-cart">Add To Cart</button>
-                            `;
+    toyCard.innerHTML = `<div class="toy-details" id="toy-name">${toyName}</div>
+                        <img src = "${toyImage}" class="toy-image">
+                        <div class="toy-details" id="toy-price">Ksh: ${toyPrice} /-</div>
+                        <button type="button" class="btn" id="view-toy">View Toy</button>
+                        <button type="button" class="btn" id="add-to-cart">Add To Cart</button>
+                        `;
+
+    let addToCartBtn = toyCard.querySelector('#add-to-cart');
+    let toyObj;
+
+
+    function addToCart(toyObj){
+      fetch(`http://localhost:3000/cart`, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(toyObj)})
+      .then(res=>res.json())
+      //.then(data=>console.log(data))
+      }
+      toyObj= {
+        name: toyName,
+        image: toyImage,
+        description:longDescription,
+        price: toyPrice,
+        quantity: 1
+      }   
+
+      //console.log(addToCartBtn)
+    addToCartBtn.addEventListener('click', e=>{
+      addToCart(toyObj)
+    })
     const viewToy = toyCard.querySelector("#view-toy");
     viewToy.addEventListener("click", (e) => {
       toysDisplay.innerHTML = ``;
-      let longDescription = toy.longDescription;
+      
       let rating = toy.rating;
       toysDisplay.innerHTML = `<div class="view-toy-details">
-                                        <img src = "${toyImage}" class="toy-image-view">
-                                        <div class="toy-details" id="toy-price">Ksh: ${toyPrice} /-</div>
-                                        <div class="toy-details" id="toy-name">${toyName}</div>
-                                        <h2>Description</h2>
-                                        <p class="decription">${longDescription}</p>
-                                        <div id="rating">Rating: ${rating} </div>
-                                        <button type="button" class="btn" id="add-cart">Add To Cart</button>
-                                    </div>
-                                    <hr>
-                                    <h2>Similar Products</h2>
-                                    `;
-        // TODO add cart
-        function updateCart(){
+                                <img src = "${toyImage}" class="toy-image-view">
+                                <div class="toy-details" id="toy-price">Ksh: ${toyPrice} /-</div>
+                                <div class="toy-details" id="toy-name">${toyName}</div>
+                                <h2>Description</h2>
+                                <p class="decription">${longDescription}</p>
+                                <div id="rating">Rating: ${rating} </div>
+                                <form id="cart-form">
+                                  <label for="quantity">Quantity: </label>
+                                  <input type="number" id="quantity" value=1 required>
+                                  <input type="submit" class="btn" id="add-cart" value="Add To Cart">
+                                </form>
+                                <button type="button" class="btn" id="view-cart">View Cart</button>
+                               </div>
+                               <div id="comments-div">
+                                <form id="comment-form">
+                                  <label for="comments-area">Add comment: </label><br>
+                                    <textarea id="comments-area" required></textarea><br>
+                                    <input type="submit" class="btn" id="add-comment" value="Comment">
+                                  </form>
+                                  <h3>Comments</h3>
+                                  <ul id="comments">
+                                  </ul>
+                               </div 
+                               <hr>
+                               <h2>Similar Products</h2>`;
+      
+        //console.log(toyObj)
+        let addcart = toysDisplay.querySelector('#cart-form');
+        let viewCart = toysDisplay.querySelector('#view-cart');
+        let commentForm = toysDisplay.querySelector('#comment-form');
+        let comments = toysDisplay.querySelector('#comments');
 
+
+        addcart.addEventListener('submit', e=>{
+          e.preventDefault();
+          let quantity = toysDisplay.querySelector('#quantity').value;
+          let quantityInt = parseInt(quantity, 10);
+          //console.log(typeof quantityInt);
+          toyObj= {
+            name: toyName,
+            image: toyImage,
+            description:longDescription,
+            price: toyPrice,
+            quantity: quantityInt
+          }
+          if (toyObj.name != cartToyName){
+            addToCart(toyObj);
+            viewCart.style.display = "block"
+          }else{
+            alert("Product already in Cart ");
+          }
+          
+        })
+        viewCart.addEventListener('click', ()=>{
+          location.href = "./public/cart.html"
+        })
+
+        //add comments
+        commentForm.addEventListener('submit', e=>{
+          
+          let userComment = toysDisplay.querySelector('#comments-area').value;
+          e.preventDefault();
+          let commentList = document.createElement('li');
+          if (userComment.trim()!= ""){
+            commentList.textContent = userComment;
+            updateComments.push(userComment);
+            toy.comments = updateComments 
+            patchComment(toy);
+            console.log(toy.comments);
+          }
+          comments.appendChild(commentList);
+          
+        })
+
+        //function to patch comments on each toy
+        function patchComment(toyInfo){
+          fetch(`http://localhost:3000/toys/${toyInfo.id}`, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+              Accept: 'application/json'
+            },
+            body: JSON.stringify(toyInfo)
+          })
+          .then(res=>res.json())
+          .then(infoData =>{
+            //console.log(infoData)
+          })
         }
+
+
+        
     });
     toysDisplay.appendChild(toyCard);
     //console.log(toy)
